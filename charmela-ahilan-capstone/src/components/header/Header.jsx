7c0a93eccede57cc5aa4635ed848fb1f50528ff1
Charmela Ahilan <charmela6@gmail.com>
2024-05-06 21:08:19 -0400

    header debug

--- charmela-ahilan-capstone/src/components/header/Header.jsx
+++ charmela-ahilan-capstone/src/components/header/Header.jsx
@@ -31,19 +31,6 @@ export default function Header() {
     <nav className="navbar">
       <div className="container">
         <div className="logo">Daily Journal</div>
-        <div className="menu-icon" onClick={handleShowNavbar}>
-          <svg
-            className="my-svg"
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="25"
-            height="25"
-            viewBox="0 0 50 50"
-          >
-            <path d="M 3 9 A 1.0001 1.0001 0 1 0 3 11 L 47 11 A 1.0001 1.0001 0 1 0 47 9 L 3 9 z M 3 24 A 1.0001 1.0001 0 1 0 3 26 L 47 26 A 1.0001 1.0001 0 1 0 47 24 L 3 24 z M 3 39 A 1.0001 1.0001 0 1 0 3 41 L 47 41 A 1.0001 1.0001 0 1 0 47 39 L 3 39 z"></path>
-          </svg>
-        </div>
         <div className={`nav-elements  ${showNavbar && "active"}`}>
           {!token && (
             <ul>
